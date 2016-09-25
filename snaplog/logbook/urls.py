642a8f138fd45e3b0c581f3bf24be03110befc4d
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-09-25 20:17:17 +0800

    Add login response view

--- snaplog/logbook/urls.py
+++ snaplog/logbook/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'logbook'
 urlpatterns = [
+    # ex: /form/
     url(r'^$', views.index, name='index'),
+    # ex: /form/login
+    url(r'^login/$', views.login, name='login'),
 ]
--- snaplog/logbook/views.py
+++ snaplog/logbook/views.py
@@ -9,3 +9,6 @@ from .models import Student
 def index(request):
     context = {}
     return render(request, 'logbook/index.html', context)
+
+def login(request):
+    return HttpResponse('Login response')

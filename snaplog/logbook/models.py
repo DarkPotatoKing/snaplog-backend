eace78465103225e4adfd89957a449f921864db0
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-09-25 18:19:50 +0800

    Add Student model

--- snaplog/logbook/models.py
+++ snaplog/logbook/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    student_number = models.PositiveIntegerField(default=0)

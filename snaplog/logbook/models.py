230fccf29d59936b4cba71aae77f127d7f28362c
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-09-25 18:56:06 +0800

    Add UNIQUE constrain to student_number

--- snaplog/logbook/models.py
+++ snaplog/logbook/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    student_number = models.PositiveIntegerField(default=0)
+    student_number = models.PositiveIntegerField(default=0, unique=True)
 
     def __str__(self):
         return '{} {} ({})'.format(self.first_name, self.last_name, str(self.student_number))
